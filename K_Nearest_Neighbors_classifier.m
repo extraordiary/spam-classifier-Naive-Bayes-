ab33cdebc8c98093141d947d459579342455ddbf
Wu Fan <37827179+extraordiary@users.noreply.github.com>
2018-10-31 15:17:43 +0800

    Update K_Nearest_Neighbors_classifier.m

--- K_Nearest_Neighbors_classifier.m
+++ K_Nearest_Neighbors_classifier.m
@@ -1,6 +1,6 @@
-classifier4bi
-classifier4log
-classifier4Z
+K_Nearest_Neighbors_classifier_bi
+K_Nearest_Neighbors_classifier_log
+K_Nearest_Neighbors_classifier_Z
 figure(4)
 plot(K,errorratebi_train,'r',K,errorratebi_test,'--r',K,errorratelog_train,'k',K,errorratelog_test,'--k',K,errorrateZ_train,'b',K,errorrateZ_test,'--b')
 title(' Plots of training and test error rates versus K')
