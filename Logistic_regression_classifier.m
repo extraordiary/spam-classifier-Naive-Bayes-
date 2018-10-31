88eb4670f2f2b19febadf0210460bd90996cc249
Wu Fan <37827179+extraordiary@users.noreply.github.com>
2018-10-31 15:16:40 +0800

    Update Logistic_regression_classifier.m

--- Logistic_regression_classifier.m
+++ Logistic_regression_classifier.m
@@ -1,9 +1,9 @@
-classifier3bi
-classifier3log
-classifier3Z
+Logistic_regression_classifier_bi
+Logistic_regression_classifier_log
+Logistic_regression_classifier_Z
 figure(4)
 plot(lambda,errorratebi_train,'r',lambda,errorratebi_test,'--r',lambda,errorratelog_train,'k',lambda,errorratelog_test,'--k',lambda,errorrateZ_train,'b',lambda,errorrateZ_test,'--b')
-title(' Plots of training and test error rates versus ¦Ë')
-xlabel('¦Ë')
+title(' Plots of training and test error rates versus Â¦Ã‹')
+xlabel('Â¦Ã‹')
 ylabel('errorrate')
 legend('errorratebi-train','errorratebi-test','errorratelog-train','errorratelog-test','errorrateZ-train','errorrateZ-test')
